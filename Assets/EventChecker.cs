db07b99d256552f82fa0cd6fb1742468074b07aa
BlueTree <bongtawo@naver.com>
2017-11-03 14:08:38 +0900

    Initial commit

---
+++ Assets/EventChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class EventChecker : MonoBehaviour
+{
+    public GameObject eventNpc;
+
+    private PlayableDirector npcDirector1;
+
+    // Use this for initialization
+    void Start()
+    {
+        npcDirector1 = eventNpc.GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("이벤트 발생");
+        npcDirector1.Play();
+    }
+}
