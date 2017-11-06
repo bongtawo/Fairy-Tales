a2861e5d58d93ff736fe1292789795b387ea8cfe
BlueTree <bongtawo@naver.com>
2017-11-06 14:30:16 +0900

    highlight tree

---
+++ Assets/CamRayScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRayScript : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, 50f))
+        {
+            if (hit.transform.CompareTag("TreeObject"))
+            {
+                hit.collider.gameObject.GetComponent<TreeHighLight>().highLighted = true;
+            }
+        }
+    }
+}
---
+++ Assets/TreeHighLight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeHighLight : MonoBehaviour
+{
+    [HideInInspector] public bool highLighted;
+
+    private void Start()
+    {
+        highLighted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var shader1 = Shader.Find("Self-Illumin/Diffuse");
+        var shader2 = Shader.Find("Diffuse");
+
+        if (highLighted == true)
+            GetComponent<Renderer>().material.shader = shader1;
+        else
+            GetComponent<Renderer>().material.shader = shader2;
+    }
+
+    private void LateUpdate()
+    {
+        highLighted = false;
+    }
+}
