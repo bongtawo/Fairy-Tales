cd4ec2160c2a9599212cce07d54c66380d837c14
BlueTree <bongtawo@naver.com>
2017-11-07 10:07:58 +0900

    thorn tree added

--- Assets/CutTreeScript.cs
+++ Assets/CutTreeScript.cs
@@ -6,6 +6,8 @@ public class CutTreeScript : MonoBehaviour
 {
     public Animation AxeAni;
 
+    private GameObject thornObject;
+
     // Update is called once per frame
     private void Update()
     {
@@ -20,14 +22,31 @@ public class CutTreeScript : MonoBehaviour
         if (other.gameObject.CompareTag("TreeObject") && AxeAni.isPlaying)
         {
             Debug.Log("나무 맞음");
-            other.gameObject.SetActive(false);
-            StartCoroutine(ResetTree(other.gameObject));
+
+            thornObject = other.transform.GetChild(0).gameObject;
+            if (thornObject.activeInHierarchy)
+            {
+                thornObject.SetActive(false);
+            }
+            else
+            {
+                other.gameObject.SetActive(false);
+                StartCoroutine(ResetTree(other.gameObject, thornObject));
+            }
         }
     }
 
-    private IEnumerator ResetTree(GameObject go)
+    private IEnumerator ResetTree(GameObject go, GameObject thorn)
     {
         yield return new WaitForSeconds(3);
+        if (Random.Range(0, 2) == 1)
+        {
+            thorn.SetActive(true);
+        }
+        else
+        {
+            thorn.SetActive(false);
+        }
         go.SetActive(true);
     }
 }
