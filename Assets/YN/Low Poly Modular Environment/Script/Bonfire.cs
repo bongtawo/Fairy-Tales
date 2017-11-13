746cd1414054c94ba5d90ea42ad06bdcb872ca8d
cspark <bidaningu@gmail.com>
2017-11-13 12:15:34 +0900

    Scene#05 폭포 추가

---
+++ Assets/YN/Low Poly Modular Environment/Script/Bonfire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    private Light light;
+    private float lastUpdate;
+    private float desiredRange;
+    public float updateFrequency = 0.15f;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastUpdate > updateFrequency)
+        {
+            lastUpdate = Time.time;
+            desiredRange = Random.Range(5f, 10f);
+        }
+
+        light.range = Mathf.Lerp(light.range, desiredRange, 0.1f);
+    }
+}
