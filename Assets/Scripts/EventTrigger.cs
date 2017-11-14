83825a47bda60a2bb2630a3b0f6f22f9656d65e6
cspark <bidaningu@gmail.com>
2017-11-14 17:42:31 +0900

    Scene#02 늑대 등장 Cinema Event 추가

---
+++ Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class EventTrigger : MonoBehaviour 
+{
+	public GameObject wolf;
+	public Renderer axeRender;
+	public GameObject mainCamera;
+	public GameObject vCamera;
+	public PlayableDirector director;
+
+	private void Start() 
+	{
+		mainCamera.SetActive(true);
+		vCamera.SetActive(false);
+		wolf.SetActive(false);
+		director.Stop();
+	}
+
+	private void Awake() 
+	{
+	}
+
+	private void OnTriggerEnter(Collider other) 
+	{
+		
+		mainCamera.SetActive(false);
+		axeRender.enabled = false;
+		wolf.SetActive(true);
+		vCamera.SetActive(true);
+		Debug.Log("Play");
+		director.Play();
+	}
+}
