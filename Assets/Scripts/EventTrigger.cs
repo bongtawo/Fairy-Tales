8314e32e64a2e19f5f4d4c2ed9c9e3db85a60b30
cspark <bidaningu@gmail.com>
2017-11-15 15:10:34 +0900

    Scene#02 Cinemachine 제거

--- Assets/Scripts/EventTrigger.cs
+++ Assets/Scripts/EventTrigger.cs
@@ -5,20 +5,15 @@ using UnityEngine.Playables;
 
 public class EventTrigger : MonoBehaviour 
 {
-	public GameObject canvas;
 	public GameObject wolf;
 	public Renderer axeRender;
-	public GameObject mainCamera;
-	public GameObject vCamera;
 	public PlayableDirector director;
 
 	private void Start() 
 	{
 		Debug.Log("Start");
-		canvas.SetActive(false);
-		vCamera.SetActive(false);
 		wolf.SetActive(false);
-		//director.Stop();
+		director.Stop();
 	}
 
 	private void OnTriggerEnter(Collider other) 
@@ -28,8 +23,7 @@ public class EventTrigger : MonoBehaviour
 
 	private void EventStart()
 	{
-		//mainCamera.SetActive(false);
-		//axeRender.enabled = false;
+		axeRender.enabled = false;
 		Debug.Log("Play");
 		director.Play();
 	}
