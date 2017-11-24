283f8d79c56cf0c8655e56e6596b696149e6a68f
BlueTree <bongtawo@naver.com>
2017-11-24 16:11:11 +0900

    메인메뉴 포탈로 생성

---
+++ Assets/Scripts/MainSceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneChanger : MonoBehaviour
+{
+    public int gotoSceneNum;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch (gotoSceneNum)
+            {
+                case 0:
+                    SceneManager.LoadScene("Intro_scene");
+                    break;
+
+                case 1:
+                    Debug.Log("개발중1");
+                    break;
+
+                case 2:
+                    Debug.Log("개발중2");
+                    break;
+
+                case 3:
+                    Debug.Log("개발중3");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
